a3b4882da2e449abfbf75d5c7d1c1d3bc2bbe18a
Henrique Moraes <henriquemsm@outlook.com>
2021-01-21 11:39:26 -0300

    Estudando sobre Construtores + Prototypes + Prototype Chain

--- script.js
+++ script.js
@@ -83,8 +83,80 @@ User henrique {
 
 // OOP no JavaScript:
 
-// No JS não existem classes, todos os objetos estão linkados a um "Prototype".
+// Prototype Inheritance: Todos os objetos estão linkados a um prototype, o prototype também é um objeto que possui propriedades e metodos que são delegadas ao objeto que foi linkado ao mesmo, processo chamado de delegation. Obs: Estas propriedades/metodos não são copiadas para o objeto, apenas podem ser acessadas por eles
+
+// NO JS uma insancia herda propriedades e metodos de uma "classe" (Prototype obj). Já em linguagens que trabalham com o OOP classico as classes herdam propriedades e metodos de uma outra classe
 
 // Os Objetos são criados de maneira programatica usando Construtores () que são funções que fazem o papel de uma "classe", passando para o objeto quais propriedades eles vão carregadar:
 
 // Exemplo
+
+// Construtor(): São functions expressions e regular functions que são chamadas com o new
+
+const Aluno = function (
+  firstName,
+  mothersFamilyName,
+  birthYear,
+  course,
+  university
+) {
+  console.log(this); // Aluno {} - empty  obj
+
+  // Definindo as propriedades que serão "levadas" para as instancias do construtor
+  this.firstName = firstName;
+  this.mothersFamilyName = mothersFamilyName;
+  this.birthYear = birthYear;
+  this.course = course;
+  this.university = university;
+};
+
+// Add metodo ao prototype
+// ALuno.prototype = Aluno.prototypeOfLinkedObj
+Aluno.prototype.getAge = function () {
+  const now = new Date();
+  console.dir(now); // getFullYear metodo da propriedade protoype do construtor Date
+  this.age = now.getFullYear() - this.birthYear;
+  console.log(this.age);
+};
+
+Aluno.prototype.species = 'Homo Sapiens';
+
+const henrique = new Aluno(
+  'Henrique',
+  'Moraes',
+  1999,
+  'Eng. Controle e Automação',
+  'Puc'
+);
+
+// Execução do new **:
+
+/* 1) Cria um {} - objeto vazio
+
+   2) quando o construtor é chamado, this do construtor -> {}, força o this apontar para este objeto vazio
+
+   3) Redefine o prototype do objeto criado para o objeto armazenado na propriedade prototype do construtor (obj__proto__ : Construtor.prototype) -> Prototype chain
+
+   4) Retorna o Objeto 
+
+   ----- Este processo ocorre nos Construtores e no ES6 classes (abstração dos construtores)
+
+*/
+
+// henrique é uma instancia do construtor Alunos, assim este carrega as propriedades que foram definidas no construtor
+console.log(henrique instanceof Aluno); // true
+
+henrique.getAge(); // this -> henrique, objeto que chama o metodo getAge()
+
+// Observe que getAge não é um metodo do obj henrique, mas da propriedade prototype do seu construtor (henrique.__proto__ linkado ao Aluno.prototype)
+console.log(henrique);
+console.log(henrique.__proto__ === Aluno.prototype);
+console.log(Aluno.prototype.isPrototypeOf(Aluno)); // false
+
+// + Instancias do construtor Aluno
+const ana = new Aluno('Ana', 'Santos', 2006, 'Medicina', 'UFMG');
+ana.getAge();
+console.log(ana);
+
+// Prototype Chain: http://prntscr.com/xdkfzv
+// EX: henrique.__proto__ : Aluno.prototype -> Como Aluno.prototype é um objeto simples, este foi criado pelo construtor Object(), assim Aluno.prototype.__proto__ : Object.prototype -> Object.prototype__proto__ : null. Quando chamamos um metodo o JS vai procura-lo seguindo a ordem dos prototypes (Primeiro procura no objeto henrique, se não encontrou, procura no seu prototype Aluno.prototype, se não encontrou procura no Object.prototype, se não encontrou = error)
